78424071bfe563752ee0a7c250067b11624ea89e
languoan <842806078@qq.com>
2023-03-29 14:51:31 +0800

    feat(): init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: 'Cute',
+  description: 'Just playing around',
+  themeConfig: {
+    siteTitle: "Cute",
+    logo: "/logo.png",
+    nav: [
+      { text: 'Weekly', link: '/weekly/issue-247' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      },
+      { text: "baidu", link: "https://www.baidu.com" },
+      { text: "ChatGPT", link: "https://chat.openai.com/chat" },
+    ],
+
+    sidebar: [
+      {
+        text: 'Weekly',
+        collapsed: true,
+        items: [
+          { text: 'weekly', link: '/weekly/issue-247' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/guide/guide1' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    }
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,32 @@
+---
+layout: home
+
+hero:
+  name: Cute
+  text: The theme is the pursuit of cuteness.
+  tagline: Lorem ipsum...
+  image:
+    src: /logo.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/guide1
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+
+features:
+  - icon:
+      src: /bird.jpg
+    title: Simple and minimal, always
+    details: Lorem ipsum...
+  - icon:
+      src: /fox.jpg
+    title: Another cool feature
+    details: Lorem ipsum...
+  - icon:
+      src: /cat.jpg
+    title: Another cool feature
+    details: Lorem ipsum...
+---
